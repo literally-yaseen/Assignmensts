a7e2e26fb8810af1127631b9ec3249239375d5b8
literally-yaseen <literally.yaseen099@gmail.com>
2026-06-22 21:42:47 +0500

    Update app3.py

--- assignments/app3.py
+++ assignments/app3.py
@@ -1,7 +1,4 @@
-# Ask the user to enter a number
 number = int(input("Enter a number: "))
-
-# Check if the number is divisible by 2
 if number % 2 == 0:
     print(f"{number} is an Even number.")
 else:
