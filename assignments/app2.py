941194a179d54d13de58ad63c8ada0d4973c66d3
literally-yaseen <literally.yaseen099@gmail.com>
2026-06-22 21:24:29 +0500

    Create app2.py

---
+++ assignments/app2.py
@@ -0,0 +1,8 @@
+# Accept age from the user
+age = int(input("Enter your age: "))
+
+# Check voting eligibility
+if age >= 18:
+    print("You are eligible for voting.")
+else:
+    print("You are not eligible for voting.")
