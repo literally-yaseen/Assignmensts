c63989e1b51715093855d0039d87ff46de06434b
literally-yaseen <literally.yaseen099@gmail.com>
2026-06-22 21:53:16 +0500

    Create app6.py

---
+++ assignments/app6.py
@@ -0,0 +1,7 @@
+length = float(input("Enter the length: "))
+breadth = float(input("Enter the breadth: "))
+
+if length == breadth:
+    print("It is a Square.")
+else:
+    print("It is a Rectangle.")
