fcdaec22531bce8be9219c6d5923b639d45d6a3d
arsa6789 <sajedaparveenshafi@gmail.com>
2026-08-20 20:21:09 +0500

    practiced operators

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-let num1 = parseFloat(prompt('Enter a number'))
-let operator = prompt('Enter a operator: + - * /')
-let num2 = parseFloat(prompt('Enter a number'))
+// variable and assignment, comparision and airthematic operator practice
 
-let result;
+// let num1 = parseFloat(prompt('Enter a number'))
+// let operator = prompt('Enter a operator: + - * /')
+// let num2 = parseFloat(prompt('Enter a number'))
 
-switch (operator) {
-    case "+":
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2
-        break;
-    case '*':
-        result = num1 * num2
-        bresk;
-    case '/':
-        result = num2 !== 0 ? num1 / num2 : '0 is invalid'
-    default:
-        result = "Invalid operator"
-        break;
-}
+// let result;
 
-alert(result)
+// switch (operator) {
+//     case "+":
+//         result = num1 + num2;
+//         break;
+//     case '-':
+//         result = num1 - num2
+//         break;
+//     case '*':
+//         result = num1 * num2
+//         bresk;
+//     case '/':
+//         result = num2 !== 0 ? num1 / num2 : '0 is invalid'
+//     default:
+//         result = "Invalid operator"
+//         break;
+// }
+
+// alert(result)
+
+// operators ternary 
+
+let status = 20
+
+let result = status >= 100 ? 'Gold' : status >= 60 ? 'Silver' : 'Bronze'
+console.log(result)
